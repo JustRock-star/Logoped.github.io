045248127cec99d65478773506473bf78c1859cc
JustRock-star <arman.grigoryan.ithink@gmail.com>
2025-08-12 16:12:51 +0400

    Update scriptStartPage.js

--- scriptStartPage.js
+++ scriptStartPage.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('first').onclick = function() {
+    document.getElementById('second').onclick = function() {
         window.location.href = '../ArrangeCorrectly/index.html';
     };
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('second').onclick = function() {
+    document.getElementById('first').onclick = function() {
         window.location.href = '../SpeedReadingMaterials/index.html';
     };
+
 });
