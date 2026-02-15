f7e22cf142cc9a0bf00bc9c71a320848815b2d39
JustRock-star <arman.grigoryan.ithink@gmail.com>
2026-02-15 12:23:18 +0400

    Remove unnecessary blank line in scriptStartPage.js

--- scriptStartPage.js
+++ scriptStartPage.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
